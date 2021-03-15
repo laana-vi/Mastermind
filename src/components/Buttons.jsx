95066756597308dd6105c3aa4e6f637dbe72cd40
Lana Vicovac <lana.vicovac@gmail.com>
2021-03-15 19:22:01 +0100

    fifth click logic added

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,44 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { mastermind } from "../service"
 
 const Buttons = ({ setAttempt, attempt, solution }) => {
-    const [clicks, setClicks] = useState(1)
+    const [clicks, setClicks] = useState(0)
+    let buttons = [1, 2, 3, 4, 5]
+
+    useEffect(() => {
+        if (clicks === 5) {
+            console.log(mastermind(attempt, solution))
+            console.log(attempt)
+            console.log(solution)
+            setClicks(0)
+            setAttempt([])
+        }
+    }, [setClicks, setAttempt, attempt, clicks, solution])
 
     const handleClick = (number) => {
         if (clicks <= 5) {
             setClicks(pv => pv + 1)
             setAttempt(pv => [...pv, number])
-            console.log(clicks)
-        }
-        if (clicks === 5) {
-            setAttempt(pv => [...pv, number])
-            setTimeout(() => {
-                console.log(mastermind(attempt, solution))
-                console.log(attempt)
-                console.log(solution)
-                setClicks(0)
-                setAttempt([])
-            }, 1000);
 
         }
     }
     return (
         <div>
-            <button onClick={() => {
-                handleClick(1)
-            }}>1</button>
-            <button onClick={() => {
-                handleClick(2)
-            }}>2</button>
-            <button onClick={() => {
-                handleClick(3)
-            }}>3</button>
-            <button onClick={() => {
-                handleClick(4)
-            }}>4</button>
-            <button onClick={() => {
-                handleClick(5)
-            }}>5</button>
+            {buttons.map(number => <button onClick={() => { handleClick(number) }} key={number}>{number}</button>)}
         </div>
     )
 }
